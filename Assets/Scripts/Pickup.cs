abd4e96598a27ecbc3e51e5647cab1edd2f3d43a
Joshua Young <j62young@gmail.com>
2018-06-02 11:40:58 -0500

    created pickup stuff

---
+++ Assets/Scripts/Pickup.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+
+public enum PickupType { weapon, boost, powerup };
+
+public class Pickup : MonoBehaviour
+{
+    public void Init(PickupType ptype, float setDuration)
+    {
+        _pickupType = ptype;
+        _duration = setDuration;
+    }
+
+    public void Apply()
+    {
+
+    }
+
+    private PickupType _pickupType;
+    private float _duration;
+    private Vector3 _spawnPosition;
+    private bool _consumed;
+}
---
+++ Assets/Scripts/SpawnManager.cs
@@ -0,0 +1,109 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SpawnManager : MonoBehaviour {
+    List<GameObject> pickups = new List<GameObject>();
+   
+    #region Mono
+
+    void Awake()
+    {
+        int locationCount = SpawnLocations.transform.childCount;
+        for (int i = 0; i < locationCount; i++)
+        {
+            GameObject foundObject = SpawnLocations.transform.GetChild(i).gameObject;
+            locations.Add(new Location(foundObject));
+            foundObject.SetActive(false);
+        }
+    }
+
+    void Start()
+    {
+        Spawn(PickupType.weapon);
+        Spawn(PickupType.boost);
+        Spawn(PickupType.powerup);
+        Spawn(PickupType.weapon);
+        Spawn(PickupType.boost);
+        Spawn(PickupType.powerup);
+        Spawn(PickupType.weapon);
+        Spawn(PickupType.boost);
+        Spawn(PickupType.powerup);
+    }
+
+	// Update is called once per frame
+	void Update () {
+		
+	}
+    #endregion
+
+    #region Public Declarations
+    public GameObject WeaponPrefab;
+    public GameObject BoostPrefab;
+    public GameObject PowerupPrefab;
+    private List<Location> locations = new List<Location>();
+    public GameObject SpawnLocations;
+    #endregion
+
+    #region Public Methods
+    public void Spawn(PickupType someType)
+    {
+        GameObject selectedPrefab = null;
+
+        if (someType == PickupType.weapon)
+        {
+            selectedPrefab = WeaponPrefab;
+        }
+        else if (someType == PickupType.boost)
+        {
+            selectedPrefab = BoostPrefab;
+        }
+        else if (someType == PickupType.powerup)
+        {
+            selectedPrefab = PowerupPrefab;
+        }
+
+        GameObject newPickup = Instantiate(selectedPrefab);
+        newPickup.transform.SetParent(transform);
+        newPickup.transform.localPosition = GetAvailablePosition();
+
+
+        Pickup weapon = newPickup.GetComponent<Pickup>();
+        weapon.Init(someType, 0.5f);
+
+        pickups.Add(newPickup);
+    }
+
+    private Vector3 GetAvailablePosition()
+    {
+        Vector3 availablePosition = Vector3.zero;
+
+        Location someLocation = locations.FindLast(s => s.available);
+
+        if (someLocation != new Location())
+        {
+            Location foundLocation = someList[0];
+            availablePosition = foundLocation.actualObj.transform.localPosition;
+            foundLocation.available = false;
+        }
+        else
+        {
+            Debug.Log("No spawn positions found!!");
+        }
+
+        return availablePosition;
+    }
+    #endregion
+}
+
+public struct Location
+{
+    public GameObject actualObj;
+    public bool available;
+
+    public Location(GameObject obj)
+    {
+        actualObj = obj;
+        available = true;
+    }
+}
