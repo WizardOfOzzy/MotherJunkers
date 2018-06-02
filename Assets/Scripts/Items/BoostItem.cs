ae21def748b2fb2b407e961ff531f219f22910c2
Brent Osborne <wizardofozzy@gmail.com>
2018-06-02 11:37:58 -0500

    Initial commit of items

---
+++ Assets/Scripts/Items/BoostItem.cs
@@ -0,0 +1,10 @@
+﻿using UnityEngine;
+
+public class BoostItem : Item 
+{
+    public const float MIN_BOOST = 0;
+    public const float MAX_BOOST = 100;
+
+    [Range(MIN_BOOST, MAX_BOOST)]
+    public float BoostAmount;
+}
---
+++ Assets/Scripts/Items/ItemController.cs
@@ -0,0 +1,48 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ItemController : MonoBehaviour 
+{
+	private void OnTriggerEnter(Collider other)
+	{
+        Item item = other.GetComponentInChildren<Item>();
+
+        if (item != null)
+        {
+            AddPickup(item);
+        }
+	}
+
+	private void AddPickup(Item item)
+    {
+        // Embrace the jank
+        if (item is Weapon)
+        {
+            AddWeapon(item as Weapon);
+        }
+        else if (item is BoostItem)
+        {
+            AddBoost(item as BoostItem);
+        }
+        else if (item is SpecialItem)
+        {
+            AddSpecial(item as SpecialItem);
+        }
+    }
+
+    private void AddWeapon(Weapon weapon)
+    {
+        // TODO - add weapon to WeaponController, re-parent Weapon GameObject
+    }
+
+    private void AddBoost(BoostItem item)
+    {
+        // TODO - add boost to BoostController, destroy Boost GameObject
+    }
+
+    private void AddSpecial(SpecialItem item)
+    {
+        // TODO - do whatever with our QuadDamage, destroy Special GameObject
+    }
+}
