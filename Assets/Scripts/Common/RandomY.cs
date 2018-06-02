314df871a79d0c25496fd60a1f8f93735c795aa1
KyleIsSuperCool <kylereichhart@yahoo.com>
2018-06-02 17:03:26 -0500

    Random Y update
    Random

--- Assets/Scripts/Common/RandomY.cs
+++ Assets/Scripts/Common/RandomY.cs
@@ -3,9 +3,12 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class RandomY : MonoBehaviour {
+    
 
 	// Use this for initialization
 	void Start () {
+
+        transform.Rotate(new Vector3(0,1,0),Random.Range(0, 360));
 		
 	}
 	
