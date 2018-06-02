f99da6411751261a4828b76c804173eac4b95d60
Michael <michael.m.goss@gmail.com>
2018-06-02 16:57:56 -0500

    Added Stock UI

---
+++ Assets/Scripts/UI/StockUI.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+
+public class StockUI : MonoBehaviour
+{
+    public Transform Panel;
+
+    public void SetStockAmmount(int ammount)
+    {
+        for (int i = 0; i < Panel.childCount; i++)
+        {
+            Image image = Panel.GetChild(i).GetComponent<Image>();
+            Color color = image.color;
+            color.a = i <= (ammount - 1) ? 1.0f : 0.0f;
+
+            image.color = color;
+        }
+    }
+}
