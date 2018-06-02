c4d31ac25185e1efd75e7fdeba541e94e365c6a5
KyleIsSuperCool <kylereichhart@yahoo.com>
2018-06-02 13:03:35 -0500

    Initial Trap commit
    Initial Trap commit

---
+++ Assets/Traps/TheTrap.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TheTrap : MonoBehaviour {
+
+    Animation OptionalAnimationClip;
+	
+
+	// Use this for initialization
+	void Start () {
+
+        OptionalAnimationClip = GetComponent<Animation>();
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+    public void ActivateTrap()
+    {
+        if (OptionalAnimationClip != null)
+            OptionalAnimationClip.Play();
+    }
+}
---
+++ Assets/Traps/TheTrigger.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TheTrigger : MonoBehaviour {
+
+    TriggerTrap MyTriggerTrap;
+
+    public int AmountOfTriggers = 10000;
+
+    bool bCanTrigger;
+
+	// Use this for initialization
+	void Start () {
+        MyTriggerTrap = GetComponentInParent<TriggerTrap>();
+    }
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+    void OnTriggerEnter(Collider other)
+    {
+        if (AmountOfTriggers <= 0 || !bCanTrigger)
+            return;
+
+        --AmountOfTriggers;
+
+        bCanTrigger = false;
+
+        MyTriggerTrap.ActivateAllTraps();
+    }
+
+    void OnTriggerExit(Collider other)
+    {
+        bCanTrigger = true;
+    }
+
+
+    }
